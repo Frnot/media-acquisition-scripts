cca6de92b6e1053880ae50efe9f70c95c865c1e9
Frnot <frnot@frnot.com>
2022-09-22 14:37:31 -0500

    improve performace greaty using inotifywait hack

--- music_downloader/autodownloader.py
+++ music_downloader/autodownloader.py
@@ -1,89 +1,87 @@
-#v1.0
+#v2.0
 
 # TODO: add setup.py or something that will install from git with pip
 
 import os
 import re
-import time
-
-from watchdog.observers import Observer
-from watchdog.events import FileSystemEventHandler
-
-from watchdog.observers.polling import PollingObserver
+import subprocess
 
 from fix_music_tags import update_dir
 from tidal_downloader import download
 
-# Files that have been modified and should be checked
-files_to_check = set()
+ignore = set()
 
 def main():
-
     # TODO: make this an argument
-    watch_directory = "/mnt/media/audio/Music"
+    ssh_string = "acq@fileserver.lab.frnot.com"
+    local_path = "/mnt/media/audio/Music"
+    remote_path = "/vault/media/audio/Music"
 
-    observer = PollingObserver()
-    event_handler = Handler()
+    dif = os.path.commonpath([local_path[::-1], remote_path[::-1]])[::-1]
+    path_translation = remote_path.replace(dif, ""), local_path.replace(dif, "")
 
-    observer.schedule(event_handler, watch_directory, recursive=True)
-    observer.start()
+    watch_changes(remote_path, path_translation, ssh_string)
+
+
+def watch_changes(watch_dir, path_translation, ssh_string):
+    cmd = f"ssh {ssh_string} 'inotifywait -mr -e close_write --format '%w%f' {watch_dir}'"
+    process = subprocess.Popen(cmd, text=True, shell=True, stdout=subprocess.PIPE)
     try:
-        print(f"Watching for changes in {watch_directory}")
-        while True:
-            process_files(files_to_check)
-            time.sleep(1)
-    finally:
-        observer.stop()
-        observer.join()
+        while path := process.stdout.readline().rstrip():
+            # Translate filepath from remote to local
+            filepath = path.replace(*path_translation)
 
+            # Check conditions
+            if os.path.basename(filepath) != "get.txt":
+                continue
 
-
-class Handler(FileSystemEventHandler):
-    @staticmethod
-    def on_created(event):
-        # Give user time to add IDs to getfile
-        time.sleep(5)
-        filepath = event.src_path
-        if event.is_directory or os.path.basename(filepath) != "get.txt":
-            return 
-        if filepath in files_to_check:
-            return
+            # Check if file still exists (potentiall large delay while requests are beign processed)
+            if not os.path.exists(filepath):
+                continue
             
-        # Check if file still exists (large delay on event trigger)
-        if os.path.exists(filepath):
-            print(f"Watchdog received 'modified' event: {filepath}")
-            files_to_check.add(filepath)
+            # Prevent runaway loop
+            if filepath in ignore:
+                ignore.remove(filepath)
+                continue
+            
+            print(f"Processing file '{filepath}'")
+            process_file(filepath)
+    finally:
+        # end inotifywait
+        pass
 
 
-
-def process_files(files):
-    for file in files:
-        newfile = ""
-        with open(file, "r") as f:
-            for line in f.readlines():
-                if not (line := line.strip()):
-                    continue
-                if id := parse_line(line):
-                    download_target = os.path.dirname(file)
-                    stat = download(id, download_target)
-                    if not stat:
-                        newfile += f"Error: could not download '{line}'\n"
-                    else:
-                        update_dir(stat)
-                else:
-                    newfile += f"Error: could not interpret '{line}'\n"
-        
-        if newfile:
-            fout = open(file, "w")
-            fout.write(newfile)
-            fout.close()
-        else:
-            os.remove(file)
-
-    # clear filename set
-    files.clear()
+def process_file(file):
+    try:
+        if os.path.getsize(file) == 0:
+            print("empty file!")
+            return
+    except (OSError, TypeError):
+        return
     
+    newfile = ""
+    with open(file, "r") as f:
+        for line in f.readlines():
+            if not (line := line.strip()):
+                continue
+            if id := parse_line(line):
+                download_target = os.path.dirname(file)
+                stat = download(id, download_target)
+                if not stat:
+                    newfile += f"Error: could not download '{line}'\n"
+                else:
+                    update_dir(stat)
+            else:
+                newfile += f"Error: could not interpret '{line}'\n"
+    
+    if newfile:
+        with open(file, "w") as f:
+            f.write(newfile)
+        ignore.add(file)
+    else:
+        os.remove(file)
 
+    
 
 def parse_line(line):
     # TODO: make regex more robust
